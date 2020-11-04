fc66db324ec2d1b70cf4e0b1940872dc2d4efaad
Leonardo Armellin <larmelli@e4r9p2.42roma.it>
2020-11-04 18:02:08 +0100

    80%

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larmelli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 13:08:13 by larmelli          #+#    #+#             */
+/*   Updated: 2020/11/02 18:04:11 by larmelli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, power - 1));
+	return (nb);
+}
